0d1ac393b614449a40fc6e41cd107a23520a4028
IversonMamangun <177076443+IversonMamangun@users.noreply.github.com>
2025-11-13 17:03:49 +0800

    first commit

---
+++ App/views/partials/nav.php
@@ -0,0 +1,45 @@
+<nav class="bg-white bg-opacity-90 fixed top-6 left-1/2 transform -translate-x-1/2 w-[95%] z-50 rounded-xl shadow-lg border border-gray-200 mt-5">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2 px-4">
+    <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="public/assets/footerlogo.png" class="h-9" alt="Logo of Filipino Investor">
+        <span class="self-center text-xl text-heading font-semibold whitespace-nowrap"></span>
+    </a>
+    <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <a href="login" class="text-blue-900 rounded-md px-3 py-1.5 text-sm font-medium">Log in</a>
+        <button type="button" class="text-white bg-[#033E94] hover:bg-[#022e6f] box-border rounded-xl focus:ring-4 focus:ring-blue-300 shadow-xs text-sm leading-5 px-5 py-2 focus:outline-none">
+        Join us
+        </button>
+        <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-sticky" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
+        </button>
+    </div>
+    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+      <ul class="flex flex-col p-2 md:p-0 mt-2 font-normal border border-default rounded-base bg-neutral-secondary-soft md:space-x-4 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-neutral-primary">
+          <a href="#" class="block py-1.5 px-2 text-s text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Home</a>
+        </li>
+        <li>
+          <a href="#" class="block py-1.5 px-2 text-s text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">About Us</a>
+        </li>
+        <li>
+          <a href="#" class="block py-1.5 px-2 text-s text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Program & Services</a>
+        </li>
+        <li>
+          <a href="#" class="block py-1.5 px-2 text-s text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Strategic Plans 2026-2027</a>
+        </li>
+        <li>
+          <a href="#" class="block py-1.5 px-2 text-s text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Testimonials</a>
+        </li>                
+        <li>
+          <a href="#" class="block py-1.5 px-2 text-s text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">News & Updates</a>
+        </li>
+        <li>
+          <a href="#" class="block py-1.5 px-2 text-s text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Gallery</a>
+        </li>
+        <li>
+          <a href="#" class="block py-1.5 px-2 text-s text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Contact Us</a>
+        </li>                
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+include_once("App/views/partials/head.php");
+include_once("App/views/partials/nav.php");
+?>
+<div class="relative">
+  <img src="public/assets/bg1.jpg" alt="" class="w-full h-auto rounded-b-[18%]">
+  
+<div class="absolute inset-0 flex flex-col items-center justify-center px-4 text-center">
+  <h1 class="text-white text-3xl md:text-5xl font-bold drop-shadow-lg mb-4">
+    FILIPINO INVESTORS SOCIETY MULTI-PURPOSE COOPERATIVE 
+    <span class="text-[#DE9F1B]">(FISMPC)</span>
+  </h1>
+  <!-- <p class="text-white text-sm md:text-base drop-shadow-md"> -->
+  <p class="text-white text-sm md:text-base max-w-6xl drop-shadow-md">
+    Welcome to the Filipino Investors Society Multi-Purpose Cooperative (FISMPC)! We are a dynamic community of visionary inventors, innovators, scientists, and entrepreneurs working together to transform Filipino ingenuity into engines of inclusive national development. Since our founding in 2011, FISMPC has become a recognized platform for innovation commercialization, bridging the gap between creative ideas and real-world solutions. Our members have developed technologies in fields like agriculture, renewable energy, education, and digital transformation. Through a decade of service and advocacy, FISMPC stands as a symbol of Filipino resilience and creativity, proving that innovation, when nurtured through cooperation, can make the Philippines great again.
+  </p>
+</div>
+
+</div>
